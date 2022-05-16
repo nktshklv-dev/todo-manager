169fd019a557e73db8b79b9b97968dad81ef6628
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-05-17 00:23:53 +0300

    created TableViewController Code

--- To-Do Manager/TableViewController.swift
+++ To-Do Manager/TableViewController.swift
@@ -11,67 +11,123 @@ class TableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        loadTasks()
     }
+    private func loadTasks(){
+        sectionsTypesPosition.forEach{
+            taskType in tasks[taskType] = []
+        }
+        tasksStorage.loadTasks().forEach{ task in
+            tasks[task.type]?.append(task)
+            
+        }
+        
+    }
+    var tasksStorage: TaskStorageProtocol = TasksStorage()
+    var tasks: [TaskPriority: [TaskProtocol]] = [:]
+    var sectionsTypesPosition: [TaskPriority] = [.important, .normal]
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return tasks.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        let taskType = sectionsTypesPosition[section]
+        guard let currentTasksType = tasks[taskType] else{
+            return 0
+        }
+        return currentTasksType.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        return getConfiguredTaskCell_constraints(for: indexPath)
     }
-    */
+    
+    private func getConfiguredTaskCell_constraints(for indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCellConstraints", for: indexPath)
+        let taskType = sectionsTypesPosition[indexPath.section]
+        guard let currentTask = tasks[taskType]?[indexPath.row] else{
+            return cell
+        }
+        let symbolLabel = cell.viewWithTag(1) as? UILabel
+        let textLabel = cell.viewWithTag(2) as? UILabel
+        
+        symbolLabel?.text = getSymbolForTask(with: currentTask.status)
+        textLabel?.text = currentTask.title
+        if currentTask.status == .planned{
+            textLabel?.textColor = .black
+            symbolLabel?.textColor = .black
+        } else{
+            textLabel?.textColor = .lightGray
+            symbolLabel?.textColor = .lightGray
+        }
+        return cell
+        
+        
+    }
+    
+    private func  getSymbolForTask(with status: TaskStatus) -> String{
+        var  resultSymbol: String
+        if status == .planned{
+            resultSymbol = "\u{25CB}"
+        }
+        else if status == .completed{
+            resultSymbol = "\u{25C9}"
+        }
+        else{
+            resultSymbol = " "
+        }
+        return resultSymbol
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        var title: String?
+        let tasksType = sectionsTypesPosition[section]
+        if tasksType == .important{
+                title = "Важные"
+        }
+        else if tasksType == .normal{
+            title = "Текущие"
+        }
+        return title
+    }
+    
 
     /*
-    // Override to support conditional editing of the table view.
+     Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
+         Return false if you do not want the specified item to be editable.
         return true
     }
     */
 
     /*
-    // Override to support editing the table view.
+     Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+             Delete the row from the data source
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+             Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
     */
 
     /*
-    // Override to support rearranging the table view.
+     Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
 
     }
     */
 
     /*
-    // Override to support conditional rearranging of the table view.
+     Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
+         Return false if you do not want the item to be re-orderable.
         return true
     }
     */
---
+++ View/Cells/TaskCell.swift
@@ -0,0 +1,14 @@
+//
+//  TaskCell.swift
+//  To-Do Manager
+//
+//  Created by Nikita  on 5/16/22.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    @IBOutlet var symbol: UILabel!
+    @IBOutlet var title: UILabel!
+}
