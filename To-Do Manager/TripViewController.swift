e58e7d1e86ecb7c3dd53075fc00f0248e60a2922
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-05-22 11:57:48 +0300

    created function for completing tasks

--- To-Do Manager/TripViewController.swift
+++ To-Do Manager/TripViewController.swift
@@ -163,5 +163,25 @@ class TripViewController: UITableViewController {
         
     }
     
-    
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let taskType = sectionsTypesPosition[indexPath.section]
+        guard let _ = tasks[taskType]?[indexPath.row] else{
+            return nil
+        }
+        
+        guard tasks[taskType]![indexPath.row].status == .completed else{
+            return nil
+        }
+        
+        
+        let action = UIContextualAction(style: .normal, title: "Plan", handler: {_,_,_ in
+            self.tasks[taskType]![indexPath.row].status = .planned
+            tableView.reloadSections(IndexSet(arrayLiteral: indexPath.section), with: .automatic)
+            
+        })
+      
+        
+        return UISwipeActionsConfiguration(actions: [action])
+        
+    }
 }
